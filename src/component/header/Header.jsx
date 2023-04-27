b5d14aafb139b56d4a83525ae2f4f230994ba11b
shahhilag4 <shahhilag16@gmail.com>
2023-04-28 03:50:13 +0530

    Celerity Task Completed

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -10,12 +10,14 @@ function Header() {
         <div className='logo'>
             <h3 className='black'>Ship</h3><h3 className='blue'>mate.</h3>
         </div>
-        <Link to='/booking'>
+        <Link to='/listing'>
         
         <button type="button" id='login' className="btn-login"
               >$</button>
-              </Link>
+        </Link>
+        <Link to='/booking'>
         <button type="button" id='learn' className="btn-learn">Track Shipment</button>  
+        </Link>
     </div>
   )
 }
